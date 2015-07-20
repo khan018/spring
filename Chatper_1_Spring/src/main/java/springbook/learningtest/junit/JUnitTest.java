f33a029546497bb383119788cbf776bc2aaf3cce
khan018 <khan018@sk.com>
2015-07-20 15:43:59 +0900

    test

---
+++ Chatper_1_Spring/src/main/java/springbook/learningtest/junit/JUnitTest.java
@@ -0,0 +1,33 @@
+package springbook.learningtest.junit;
+
+//import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.junit.Assert.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+public class JUnitTest {
+	static Set<JUnitTest> testObjects = new HashSet<JUnitTest>();
+	
+	@Test public void test1() {
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
+	}
+	
+	@Test public void test2() {
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
+	}
+	
+	@Test public void test3() {
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
+	}
+}
+
